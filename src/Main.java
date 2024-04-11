b36a3c9b29375f7d9f67e0d6946609ff0cf9c17b
AchrafIbrahimi <achrafibrahimi.1998@gmail.com>
2024-04-11 19:50:36 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
+
+    public static void Achraf(){
+        for (int i=0;i<100;i++){
+            System.out.print("Achraf__");
+        }
+    }
+    public static void Ibrahimi(){
+        for (int i=0;i<100;i++){
+            System.out.print("Ibrahimi__");
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Achraf();
+        System.out.println();
+        Ibrahimi();
+
     }
 }
+
